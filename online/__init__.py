e235367c50ca450d5fb04d64617e9e25c4c647d5
YehowahLiu <redme@live.cn>
2021-08-21 14:52:42 +0800

    make it a packed plugin for mcdr 2.x

--- online/__init__.py
+++ online/__init__.py
@@ -1,22 +1,8 @@
-import socket
-import select
-import struct
-import time
 import os
 import json
 from mcdreforged.api.rtext import RAction, RText, RColor, RTextList
 
-PLUGIN_METADATA = {
-    'id': 'online',
-    'version': '1.1.0',
-    'link': 'https://github.com/FAS-Server/online',
-    'author': [
-        'A_jiuA', 'Nine_King', 'YehowahLiu'
-    ],
-    'dependencies': {
-        'mcdreforged': '>=1.0.0',
-    }
-}
+from online.RCon import MCRcon
 
 # 默认参数，不要修改
 configPath = 'config/online.json'
@@ -31,70 +17,6 @@ defultConfig = '''
         "password": "ServerAPassword"
     }
 }'''
-
-
-# mcrcon
-class MCRconException(Exception):
-    pass
-
-
-class MCRcon(object):
-    socket = None
-
-    def connect(self, host, port, password):
-        if self.socket is not None:
-            raise MCRconException("Already connected")
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        self.send(3, password)
-
-    def disconnect(self):
-        if self.socket is None:
-            raise MCRconException("Already disconnected")
-        self.socket.close()
-        self.socket = None
-
-    def read(self, length):
-        data = b""
-        while len(data) < length:
-            data += self.socket.recv(length - len(data))
-        return data
-
-    def send(self, out_type, out_data):
-        if self.socket is None:
-            raise MCRconException("Must connect before sending data")
-
-        # 发送请求包
-        out_payload = struct.pack('<ii', 0, out_type) + out_data.encode('utf8') + b'\x00\x00'
-        out_length = struct.pack('<i', len(out_payload))
-        self.socket.send(out_length + out_payload)
-
-        # 读取响应包
-        in_data = ""
-        while True:
-            # 读包
-            in_length, = struct.unpack('<i', self.read(4))
-            in_payload = self.read(in_length)
-            in_id, in_type = struct.unpack('<ii', in_payload[:8])
-            in_data_partial, in_padding = in_payload[8:-2], in_payload[-2:]
-
-            # 连接检查
-            if in_padding != b'\x00\x00':
-                raise MCRconException("Incorrect padding")
-            if in_id == -1:
-                raise MCRconException("Login failed")
-
-            # 记录的响应
-            in_data += in_data_partial.decode('utf8')
-
-            # 如果没有更多的东西要接收，返回响应
-            if len(select.select([self.socket], [], [], 0)[0]) == 0:
-                return in_data
-
-    def command(self, command):
-        result = self.send(2, command)
-        time.sleep(0.003)
-        return result
 
 
 def main(host, port, password):  # 连接服务器
